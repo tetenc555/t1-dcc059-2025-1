39d3a8dd09148569d829520eeb16af36c255f5a4
Rafael <rafaelfreesz@gmail.com>
2025-06-10 21:14:57 -0300

    Atualizanod ids dos vértices para char

--- src/Grafo.h
+++ src/Grafo.h
@@ -16,13 +16,13 @@ public:
     Grafo();
     ~Grafo();
 
-    vector<char> fecho_transitivo_direto(int id_no); // a
-    vector<char> fecho_transitivo_indireto(int id_no); // b
-    vector<char> caminho_minimo_dijkstra(int id_no_a, int id_no_b); // c
-    vector<char> caminho_minimo_floyd(int id_no, int id_no_b); // d
+    vector<char> fecho_transitivo_direto(char id_no); // a
+    vector<char> fecho_transitivo_indireto(char id_no); // b
+    vector<char> caminho_minimo_dijkstra(char id_no_a, char id_no_b); // c
+    vector<char> caminho_minimo_floyd(char id_no, char id_no_b); // d
     Grafo* arvore_geradora_minima_prim(vector<char> ids_nos); // e
     Grafo* arvore_geradora_minima_kruskal(vector<char> ids_nos); // f
-    Grafo* arvore_caminhamento_profundidade(int id_no); // g
+    Grafo* arvore_caminhamento_profundidade(char id_no); // g
     int raio(); // h 1
     int diametro(); // h 2
     vector<char> centro(); // h 3
