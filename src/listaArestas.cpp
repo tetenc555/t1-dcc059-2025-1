63791f9ebba0b49beae566920922c934551de1e8
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-06-20 13:57:09 -0300

    construtor inicial lista de arestas (nao considerei peso)

--- src/listaArestas.cpp
+++ src/listaArestas.cpp
@@ -8,5 +8,15 @@
 using namespace std;
 
 listaArestas::listaArestas(vector<No *> lista_adj, bool ehDir) {
-
+    for(int i=0;i<lista_adj.size();i++) {
+        No* no = lista_adj[i];
+        for (int j=0;j<no->arestas.size();j++) {
+            tuple <char,char> conexao = {no->id,no->arestas[j]};
+            conexoes.push_back(conexao);
+            if (!(ehDir)) {
+                tuple <char,char> conexaoVolta = {no->arestas[j],no->id};
+                conexoes.push_back(conexaoVolta);
+            }
+        }
+    }
 }
