b680056ea0b455a6d0a492738ac11d99b473e87d
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-06-30 18:30:09 -0300

    altera main para usar argc

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+#include <fstream>
 #include <iostream>
 #include "Gerenciador.h"
 #include "LeituraArquivos.h"
 using namespace std;
-int main(int argc, char *argv[])
-{
-    Grafo* grafo = LeituraArquivos::lerGrafodoArquivo("./input/grafo100.txt");
 
+int main(int argc, char *argv[]) {
+    // Caminho padrão se nenhum argumento for passado
+    string caminho = (argc > 1) ? argv[1] : "./input/grafo100.txt";
+
+    ifstream arquivoTeste(caminho);
+    if (!arquivoTeste) {
+        cerr << "Erro: Arquivo não encontrado!\n";
+        cerr << "Tentando abrir: " << caminho << "\n";
+        cerr << "Execute com: " << argv[0] << " ./input/grafo100.txt\n";
+        return 1;
+    }
+    arquivoTeste.close();
+
+    Grafo* grafo = LeituraArquivos::lerGrafodoArquivo(caminho);
     Gerenciador::comandos(grafo);
-
     return 0;
 }
