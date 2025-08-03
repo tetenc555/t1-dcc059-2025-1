08d718dca87fc53b54a9818fc4950c980c7b9eb0
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-08-03 15:20:52 -0300

    fdhfdcjf

--- src/MinWeightDominatingSet.cpp
+++ src/MinWeightDominatingSet.cpp
@@ -4,6 +4,7 @@
 
 #include "MinWeightDominatingSet.h"
 #include <iostream>
+#include <queue>
 using namespace std;
 
 MinWeightDominatingSet::MinWeightDominatingSet(Grafo* grafoInicial, int tipo) {
@@ -30,8 +31,30 @@ MinWeightDominatingSet::~MinWeightDominatingSet() {
     delete conjuntoSolucao;
 }
 
-void MinWeightDominatingSet::guloso(Grafo *grafoInicial) {
-    cout<< "teste guloso" << endl;
+void calculaPesoNoCandidatoGuloso(priority_queue<tuple<float,char>,vector<tuple<float,char>>,greater<tuple<float,char>>>* candidatos)
+{
+    for (int i=0; i < candidatos->size(); ++i)
+    {
+        tuple<float,char> noAtualizar = candidatos[0];
+
+    }
+}
+
+void MinWeightDominatingSet::guloso(Grafo *grafoInicial)
+{
+    conjuntoSolucao = new Grafo(grafoInicial->in_direcionado,grafoInicial->in_ponderado_aresta,grafoInicial->in_ponderado_vertice); //declara solucao vazia
+    priority_queue<tuple<float,char>,vector<tuple<float,char>>,greater<tuple<float,char>>> candidatos; //declara lista de candidatos
+    for (No* no : grafoInicial->lista_adj) //adicao inicial a lista de candidatos
+    {
+        float pesoProcessamento=no->peso;
+        pesoProcessamento=pesoProcessamento/no->arestas.size();
+        candidatos.emplace(pesoProcessamento,no->id);
+    }
+
+    while (conjuntoSolucao->lista_adj.size() < grafoInicial->lista_adj.size() || !candidatos.empty())
+    {
+
+    }
 }
 
 void MinWeightDominatingSet::gulosoRand(Grafo *grafoInicial) {
