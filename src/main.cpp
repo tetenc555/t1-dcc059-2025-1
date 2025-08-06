3b5035acd8211439f69ab142a3855a4a75bc5a21
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-08-06 12:16:50 -0300

    remove instancias do trabalho 1 e adiciona as do trabalho 2

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     // Caminho padrão se nenhum argumento for passado
-    string caminho = (argc > 1) ? argv[1] : "./input/grafo100.txt";
+    string caminho = (argc > 1) ? argv[1] : "./input/g_25_0.16_0_1_0.txt";
 
     ifstream arquivoTeste(caminho); //verificar se o caminho ta correto
     if (!arquivoTeste) {
         cerr << "Erro: Arquivo não encontrado!\n";
         cerr << "Tentando abrir: " << caminho << "\n";
-        cerr << "Execute com: " << argv[0] << " ./input/grafo100.txt\n";
+        cerr << "Execute com: " << argv[0] << " ./input/g_25_0.16_0_1_0.txt\n";
         return 1;
     }
     arquivoTeste.close();
